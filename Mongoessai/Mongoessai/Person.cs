ba3760115007e26459f6d413740842f66ade0ec9
ProgHorusEye <ivankleiber@hotmail.fr>
2018-06-12 14:28:10 +0200

    Initial Commit

---
+++ Mongoessai/Mongoessai/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace Mongoessai
+{
+    class Person
+    {
+        public ObjectId id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public List<string> Interests { get; set; }
+
+
+        public Person()
+        {
+        }
+    }
+}
---
+++ Mongoessai/Mongoessai/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+
+namespace Mongoessai
+{
+    class Program
+    {
+        /// <summary>
+        /// créer une person en static
+        /// </summary>
+        static Person staticPerson = new Person()
+        {
+            FirstName = "Ivan",
+            LastName = "Kleiber",
+            Age = 37,
+            Interests = new List<string>() { "Photo","Natation" }
+        };
+
+        /// <summary>
+        /// Crée une liste de personne
+        /// </summary>
+        static List<Person> staticPeople = new List<Person>()
+        {
+        new Person() {FirstName = "Kleiber", LastName="Georges", Age = 45, Interests = new List<string>(){"Natation","Photo" } },
+        new Person() { FirstName = "Jean", LastName = "Tasse", Age = 32, Interests = new List<string>() { "Foot", "Brocante" } }
+    };
+
+        static void Main(string[] args)
+        {
+            
+
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            IMongoDatabase db = client.GetDatabase("people");
+
+            IMongoCollection<Person> collection = db.GetCollection<Person>("persons");
+
+            db.DropCollection("persons"); //vide la base
+
+            collection.InsertOne(staticPerson);
+
+            collection.InsertMany(staticPeople);
+
+           
+           List<Person> result = collection.Find(person => person.FirstName == "Ivan").ToList();
+           Console.WriteLine(result.Count().ToString() + " personnes trouvées pour le filtre");
+
+            List<Person> resultall = collection.Find(x => true).ToList();
+
+            Console.WriteLine(resultall.Count().ToString() + "Personnes trouvées");
+
+            foreach (Person p in resultall)
+            {
+                Console.WriteLine(p.id + " : " + p.FirstName + " " + p.LastName + "(" + p.Age + ")" );
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
